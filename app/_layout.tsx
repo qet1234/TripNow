7a0e7dcb3869ceaa5b78ff1d467d0fa9f8d0c5c9
qet1234 <144639971+qet1234@users.noreply.github.com>
2026-08-27 11:11:24 +0900

    feat: add app/_layout.tsx

---
+++ app/_layout.tsx
@@ -0,0 +1,10 @@
+import "react-native-url-polyfill/auto";
+import { Stack } from "expo-router";
+
+export default function RootLayout() {
+  return (
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="(tabs)" />
+    </Stack>
+  );
+}
